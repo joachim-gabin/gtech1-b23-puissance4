d0161429e4853cab8363e86d7e9e314d4c3df1db
WIART Noah <nwiart@gaming.tech>
2021-10-21 18:00:31 +0200

    Win detection fully working

--- winner.c
+++ winner.c
@@ -34,13 +34,61 @@ static bool test_win_vertical( char* table, char token, int col )
 	return false;
 }
 
+// Left to right diagonal win test.
+static bool test_win_ltor( char* table, char token, int row, int col )
+{
+	int min = row < col ? row : col;
+
+	// Back up to the edge.
+	row -= min;
+	col -= min;
+
+	int count = 0;
+	while ( row < NUM_ROWS && col < NUM_COLUMNS )
+	{
+		if ( table[row * NUM_COLUMNS + col] == token ) count++;
+		else count = 0;
+
+		if ( count == 4 )
+			return true;
+
+		row++;
+		col++;
+	}
+	return false;
+}
+
+// Right to left diagonal win test.
+static bool test_win_rtol( char* table, char token, int row, int col )
+{
+	int oneminus_col = (NUM_COLUMNS - 1) - col;
+	int min = row < oneminus_col ? row : oneminus_col;
+
+	// Back up to the edge.
+	row -= min;
+	col += min;
+
+	int count = 0;
+	while ( row < NUM_ROWS && col >= 0 )
+	{
+		if ( table[row * NUM_COLUMNS + col] == token ) count++;
+		else count = 0;
+
+		if ( count == 4 )
+			return true;
+
+		row++;
+		col--;
+	}
+	return false;
+}
+
 bool test_win( char* table, char token, int row, int col )
 {
-	if ( test_win_horizontal( table, token, row ) )
-		return true;
-
-	if ( test_win_vertical( table, token, col ) )
-		return true;
+	if ( test_win_horizontal( table, token, row ) ) return true;
+	if ( test_win_vertical( table, token, col ) )   return true;
+	if ( test_win_ltor( table, token, row, col ) )  return true;
+	if ( test_win_rtol( table, token, row, col ) )  return true;
 
 	return false;
 }
--- winner.h
+++ winner.h
@@ -3,6 +3,10 @@
 
 #include "bool.h"
 
+// Tests if player won by placing a token.
+// The function expects the game table, the player's token, and the last played position.
+// It will then check for a sequence horizontally and vertically on the given row / column.
+// /!\ Diagonal check isn't implemented yet.
 bool test_win( char* table, char token, int row, int col );
 
 #endif // PUIS4_WINNER_H
